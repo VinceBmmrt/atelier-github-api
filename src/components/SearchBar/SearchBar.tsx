b77736b67b4300c24545815d615723c251722fff
Aspitrine <loquente62@gmail.com>
2023-10-31 11:45:53 +0100

    Input controler dans le composant SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,19 @@
+import { ChangeEvent, useState } from 'react';
 import { Form, Input, Segment } from 'semantic-ui-react';
 
 function SearchBar() {
+  // Je stock l'information sur la valeur de mon input dans le composant SearchBar
+  // Car seul ce composant à besoin de connaître cette information à chaque instant
+  // Le composant App voudra cette information mais uniquement à un moment précis, la soumission du formulaire
+  const [searchText, setSearchText] = useState('');
+
+  const handleChangeSearchValue = (event: ChangeEvent<HTMLInputElement>) => {
+    // Je récupère la nouvelle valeur depuis mon élément HTML (event.target)
+    const newValue = event.target.value;
+    // Je modifie la valeur de searchText avec la nouvelle valeur
+    setSearchText(newValue);
+  };
+
   return (
     <Segment>
       <Form>
@@ -10,6 +23,10 @@ function SearchBar() {
             icon="search"
             placeholder="React..."
             iconPosition="left"
+            // Je fixe la valeur de mon input à la valeur de searchText
+            value={searchText}
+            // Lorsque la valeur de l'input change, on met à jour la valeur de searchText
+            onChange={handleChangeSearchValue}
           />
         </Form.Field>
       </Form>
