766b711c7ffd8eddf646fef343245ff34b7d3569
Aspitrine <loquente62@gmail.com>
2023-10-31 11:16:15 +0100

    Stockage des infos de repos dans des variables d'état

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Image } from 'semantic-ui-react';
 import Message from '../Message/Message';
 import ReposResults from '../ReposResults/ReposResults';
@@ -9,14 +10,17 @@ import reposData from '../../data/repos';
 import './App.scss';
 
 function App() {
+  const [repos, setRepos] = useState(reposData.items);
+  const [totalCount, setTotalCount] = useState(reposData.total_count);
+
   return (
     <div className="app">
       <header className="app__header">
         <Image src={logoGithub} alt="logo github" centered size="small" />
       </header>
       <SearchBar />
-      <Message message="La recherche a donnée 1159901 résultats" />
-      <ReposResults repos={reposData.items} />
+      <Message message={`La recherche a donnée ${totalCount} résultat(s)`} />
+      <ReposResults repos={repos} />
     </div>
   );
 }
